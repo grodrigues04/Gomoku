16b8e415aa6a876fbfd9955d8005db3204ad6be3
Gustavo Bernardo <gurodrigues.b@gmail.com>
2024-06-18 13:05:58 -0300

    fix: change gameGUI heritage

--- GameGUI.py
+++ GameGUI.py
@@ -1,20 +1,23 @@
 from Board import Board
-from gameRules import judge
+from gameRules import GameRules
 
 def start():
-    playerOne = input('Digite o nome do jogador 1')
-    playerTwo = input('Digite o nome do jogador 2')
+    playerOne = input('Digite o nome do jogador 1: ')
+    playerTwo = input('Digite o nome do jogador 2: ')
     return playerOne,playerTwo
 
-class GameGUI(Board):
+class GameGUI(GameRules):
     def __init__(self, playerone, playertwo):
-        self.board = self.CreateBoard()
-        self.gameJudge = judge(Board.CreateBoard(self))
+        self.board = Board()
+        self.boardToPlay = self.board.CreateBoard()
+        super().__init__(self.boardToPlay) #passando o tabuleiro para o construtor de gameRules
         self.playerone = playerone
         self.playertwo = playertwo 
         self.turn = 0
         
     def play(self):
+        print('mainBoard do gameGUI:\n',self.boardToPlay)
+        self.board.showBoard(self.boardToPlay)
         if self.turn: 
             jogadaLinha = int(input(f'{self.playerone} digite o número da linha'))-1 #Indices de python
             jogadaColuna = int(input(f'{self.playerone} digite o número da Coluna'))-1
@@ -22,8 +25,8 @@ class GameGUI(Board):
         else:
             jogadaLinha = int(input(f'{self.playerone} digite o número da linha'))-1
             jogadaColuna = int(input(f'{self.playerone} digite o número da Coluna'))-1
-        self.gameJudge.playturn(jogadaLinha, jogadaColuna, self.turn)
-
+        self.playturn(jogadaLinha, jogadaColuna, self.turn)
+        self.board.showBoard(self.boardToPlay)
 
 #Iniciando o jogo 
 p1,p2 = start()
