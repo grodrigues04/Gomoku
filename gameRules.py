47f73bd048425ffc6b2ea31a748a5795ea6af3fd
Gustavo Bernardo <gurodrigues.b@gmail.com>
2024-06-18 13:04:59 -0300

    fix: change class config

--- gameRules.py
+++ gameRules.py
@@ -1,9 +1,9 @@
-class judge():
-    def __init__(self,tabuleiro):
+class GameRules():
+    def __init__(self, tabuleiro):
         self.tabuleiro = tabuleiro
-    def playturn(self,playerline,playercolum,turn):
+    def playturn(self, playerline, playercolum, turn):
         #TO-DO: cores individuais para cada jogador 
         if turn:
-            tabuleiro[playerline][playercolum] = '\033[32m{1}\033[0m'
+            self.tabuleiro[playerline][playercolum] = '\033[32m1\033[0m'
         else:
-            tabuleiro[playerline][playercolum] = '\033[34m{1}\033[0m'
+            self.tabuleiro[playerline][playercolum] = '\033[34m2\033[0m'
