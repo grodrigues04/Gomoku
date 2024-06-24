fc98b0e70f7ba40974ae111d3b3503a117442d04
Gustavo Bernardo <gurodrigues.b@gmail.com>
2024-06-24 13:50:47 -0300

    style: formart file with ruff

--- Board.py
+++ Board.py
@@ -1,8 +1,8 @@
-class Board():
+class Board:
     def __init__(self):
         self.line = 19
         self.colum = 19
-    
+
     def CreateBoard(self):
         mainBoard = []
         lineControl = 0
@@ -10,46 +10,44 @@ class Board():
             row = []
             columControl = 0
             while columControl < self.colum:
-                row.append(0)
-                columControl+=1
+                row.append(" ")
+                columControl += 1
             mainBoard.append(row)
-            lineControl+=1
+            lineControl += 1
         return mainBoard
-    
+
     def showBoard(self, mainBoard):
-        number = 1  
-        print('coluna: ',end='')
-        print('',f'\033[31m{number}\033[0m',end ="")
-        number+=1
-        #mostrando as colunas com numeros
+        number = 1
+        print("coluna: ", end="")
+        print("", f"\033[31m{number}\033[0m", end="")
+        number += 1
+        # mostrando as colunas com numeros
         while number <= self.line:
-            numberPrint = f'\033[31m{number}\033[0m'
+            numberPrint = f"\033[31m{number}\033[0m"
             if number <= 9:
-                print('  ',numberPrint,end ="")
+                print("  ", numberPrint, end="")
             else:
-                print(' ',numberPrint,end ="")
-            number+=1
+                print(" ", numberPrint, end="")
+            number += 1
         print()
         linhaValor = 1
-        #mostrando as linhas da matriz
+        # mostrando as linhas da matriz
         for linha in mainBoard:
-            valorPrint = f'\033[31m{linhaValor}\033[0m'
-            if linhaValor <10:
-                print(f'Linha:{valorPrint} ',end='')
+            valorPrint = f"\033[31m{linhaValor}\033[0m"
+            if linhaValor < 10:
+                print(f"Linha:{valorPrint} ", end="")
             else:
-                print(f'Linha:{valorPrint}',end='')
-            linhaValor+=1
+                print(f"Linha:{valorPrint}", end="")
+            linhaValor += 1
             for item in linha:
-                valores = f'\033[35m{item}\033[0m'
-                print(f' {valores} |',end='') 
+                valores = f"\033[35m{item}\033[0m"
+                print(f" {valores} |", end="")
             print()
-            print('         ',end='') #8 spaces
-            print('--|',end='')
+            print("         ", end="")  # 8 spaces
+            print("--|", end="")
             quadrado = 1
             while quadrado < self.line:
-                print('---|',end='')
-                quadrado+=1
+                print("---|", end="")
+                quadrado += 1
             print()
         print()
-            
-            
